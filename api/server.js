7a8c8e1911b245841e14fa888e4acfb524887221
jeiooo <angzcolao@gmail.com>
2024-03-27 14:25:17 +0800

    second commit

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(express.json());
+
+const url = ''
+
+app.listen(3000, () => {
+    console.log('Connected to Port 3000... ')
+});
