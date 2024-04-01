a3bec02d68a8b8a11199bd21d5a23bc3cbcf1ccc
jeiooo <angzcolao@gmail.com>
2024-04-01 13:57:13 +0800

    fourth commit

--- api/index.js
+++ api/index.js
@@ -6,10 +6,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const url = 'mongodb://localhost:27017/full_prac';
-mongoose.connect(url, {})
-    .then(result => console.log('Connected to DB... '))
-    .catch(err => console.log(error));
+//const url = 'mongodb://localhost:27017/full_prac';
+//mongoose.connect(url, {})
+//    .then(result => console.log('Connected to DB... '))
+//    .catch(err => console.log(error));
+
+mongoose.connect('mongodb+srv://testmongodb.fnhlghj.mongodb.net/RestAPI',
+    {
+        appName: 'testmongodb',
+        user: 'ustp_jelo',
+        pass: 'TQpmpsVB16hx2xua'
+    })
+    .then(() => {
+        console.log('MongoDB is Connected... ');
+    });
 
 const TodoRoute = require('./routes/todo.route');
 app.use(('/todos'), TodoRoute);
